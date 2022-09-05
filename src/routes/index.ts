83af7efc8c74bde71ca19f6ef41847d7033fc1f3
ruda <ruda.rabello.silva@gmail.com>
2022-09-05 14:35:56 -0300

    test: add inicial route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import cardRouter from "./cardRouter";
 import purchaseRouter from "./purchaseRouter";
 import rechargeRouter from "./rechargeRouter";
@@ -8,5 +8,6 @@ const router = Router();
 router.use(cardRouter);
 router.use(rechargeRouter);
 router.use(purchaseRouter);
+router.use("/", (req: Request, res: Response) => res.send('ok'))
 
 export default router;
